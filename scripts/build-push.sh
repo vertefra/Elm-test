ca5beecbfadd0bd883e72254e5146af7527d537c
vertefra <verte.fra@gmail.com>
2021-08-22 10:39:29 -0400

    fix docker run

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -1,3 +1,2 @@
 docker build . --tag ${REGISTRY}/${USERNAME}/${IMAGE_NAME?Variable not set} && \
-docker run ${REGISTRY}/${USERNAME}/${IMAGE_NAME?Variable not set}
 docker push ${REGISTRY}/${USERNAME}/${IMAGE_NAME?Variable not set}
